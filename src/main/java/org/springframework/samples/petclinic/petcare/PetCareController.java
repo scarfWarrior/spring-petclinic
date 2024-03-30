a959ae6afa61e43e5b7ef32c34b3447a515eedf8
scarfWarrior <106388635+scarfWarrior@users.noreply.github.com>
2024-03-30 22:10:53 +0000

    [Implementing-PetCare-Entity] - package imports

--- src/main/java/org/springframework/samples/petclinic/petcare/PetCareController.java
+++ src/main/java/org/springframework/samples/petclinic/petcare/PetCareController.java
@@ -1,3 +1,5 @@
+package org.springframework.samples.petclinic.petcare;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
--- src/main/java/org/springframework/samples/petclinic/petcare/PetCareService.java
+++ src/main/java/org/springframework/samples/petclinic/petcare/PetCareService.java
@@ -1,3 +1,5 @@
+package org.springframework.samples.petclinic.petcare;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
